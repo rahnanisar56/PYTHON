0f5fd86c3018071ba06043bf6be0bd84b3c927e6
rahnanisar <rahnanisar56@gmail.com>
2026-06-09 23:04:23 +0530

    hw day 13

---
+++ hw13.py
@@ -0,0 +1,28 @@
+import os
+
+n = int(input("How many student names do you want to add? "))
+
+file = open("student.txt", "a")
+
+for i in range(n):
+    name = input(f"Enter student name {i+1}: ")
+    file.write(name + "\n")
+
+file.close()
+
+if os.path.exists("student.txt"):
+    print("\nExisting Student Names:")
+    
+    file = open("student.txt", "r")
+    for line in file:
+        print(line.strip())
+    file.close()
+else:
+    print("\nNo existing student records found.")
+
+print("\nUpdated List of Student Names:")
+
+file = open("student.txt", "r")
+for line in file:
+    print(line.strip())
+file.close()
