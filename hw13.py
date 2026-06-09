a67fba620e9838ea7f7794f07e1b616e64faba2a
rahnanisar <rahnanisar56@gmail.com>
2026-06-09 23:06:50 +0530

    changed the txt file

--- hw13.py
+++ hw13.py
@@ -2,7 +2,7 @@ import os
 
 n = int(input("How many student names do you want to add? "))
 
-file = open("student.txt", "a")
+file = open("students.txt", "a")
 
 for i in range(n):
     name = input(f"Enter student name {i+1}: ")
@@ -10,10 +10,10 @@ for i in range(n):
 
 file.close()
 
-if os.path.exists("student.txt"):
+if os.path.exists("students.txt"):
     print("\nExisting Student Names:")
     
-    file = open("student.txt", "r")
+    file = open("students.txt", "r")
     for line in file:
         print(line.strip())
     file.close()
@@ -22,7 +22,7 @@ else:
 
 print("\nUpdated List of Student Names:")
 
-file = open("student.txt", "r")
+file = open("students.txt", "r")
 for line in file:
     print(line.strip())
 file.close()
